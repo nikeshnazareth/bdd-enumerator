16878b6097d60cc1775127879c478316b46e1cd9
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-03 18:35:59 +1100

    Remove the enumerate.all function
    The main purpose of the function is to enumerate over the properties of an object,
    ensuring the result is only valid when all properties are valid.
    
    This functionality is now achievable with the GenericObject predefined scenario

--- enumerate.js
+++ enumerate.js
@@ -4,48 +4,6 @@ const Scenario = require('./scenario');
  * A class that accepts properties and expands the possible options into a set of BDD tests
  */
 class Enumerate {
-
-    /**
-     * Creates tests for all combinations of properties, where the combination is only valid when
-     * all individual properties are valid
-     * @param properties a list of properties to test
-     * @param validTestsFn the tests to run when all properties are valid
-     * @param invalidTestsFn the tests to run when any property is invalid
-     */
-    static all(properties, validTestsFn, invalidTestsFn) {
-        if (properties.length === 0) {
-            validTestsFn();
-            return;
-        }
-
-        const property = properties[0];
-
-        describe(property.name, () => {
-            property.scenarios.map(scenario => {
-                describe(scenario.desc, () => {
-                    beforeEach(() => {
-                        if (scenario.value !== undefined) {
-                            let obj = property.baseObjFn();
-                            const path = property.name.split('.');
-                            while (path.length > 1) {
-                                const intermediate = path.shift();
-                                if (obj[intermediate] === undefined)
-                                    obj[intermediate] = {};
-                                obj = obj[intermediate];
-                            }
-                            obj[path] = scenario.value;
-                        }
-                    });
-
-                    if (scenario.valid)
-                        Enumerate.all(properties.slice(1), validTestsFn, invalidTestsFn);
-                    else
-                        Enumerate.all(properties.slice(1), invalidTestsFn, invalidTestsFn);
-                });
-            });
-        });
-    }
-
     static expandScenario(scenario, validTestsFn, invalidTestsFn, parents = []) {
         // the contents of this scenario expansion is wrapped in a function to because it may not require a describe block
         const wrapper = () => {
