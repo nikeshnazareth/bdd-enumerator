b92d3030f3780c27c583a6f50b2bdc16f3b52bca
nikeshnazareth <nikesh.nazareth.developer@gmail.com>
2018-12-04 17:13:46 +1100

    Add missing comma to boundedInteger.js

--- predefined-scenarios/boundedInteger.js
+++ predefined-scenarios/boundedInteger.js
@@ -9,7 +9,7 @@ const BoundedInteger = (lower, upper) => [
     new SimpleScenario('is a string', 'Arbitrary string', false),
     new SimpleScenario(`is less than the lower bound (${lower})`, lower - 1, false),
     new SimpleScenario(`is equal to the lower bound (${lower})`, lower, true),
-    new SimpleScenario(`is an integer (${Math.floor((upper + lower) / 2)}) in the middle of the range [${lower}, ${upper}]`, Math.floor((upper + lower) / 2), true)
+    new SimpleScenario(`is an integer (${Math.floor((upper + lower) / 2)}) in the middle of the range [${lower}, ${upper}]`, Math.floor((upper + lower) / 2), true),
     new SimpleScenario(`is equal to the upper bound (${upper})`, upper, true),
     new SimpleScenario(`is higher than the upper bound (${upper})`, upper + 1, false),
     new SimpleScenario(`is a fraction`, lower + 0.5, false)
