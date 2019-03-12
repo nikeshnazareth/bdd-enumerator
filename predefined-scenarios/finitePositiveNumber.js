edbe244aec7e330526af5bb974414f1b18b3ea50
nikeshnazareth <business@nikeshnazareth.com>
2019-03-12 20:19:38 +1100

    Expand the finite positive number scenarios

--- predefined-scenarios/finitePositiveNumber.js
+++ predefined-scenarios/finitePositiveNumber.js
@@ -11,7 +11,11 @@ const FinitePositiveNumber = [
     new SimpleScenario('is one', 1, true),
     new SimpleScenario('is another positive integer', 2, true),
     new SimpleScenario('is a positive fraction', 2.6, true),
-    new SimpleScenario('is MAX_VALUE', Number.MAX_VALUE, true)
+    new SimpleScenario('is a negative fraction', -3.6, false),
+    new SimpleScenario('is NaN', Number.NaN, false),
+    new SimpleScenario('is MAX_VALUE', Number.MAX_VALUE, true),
+    new SimpleScenario('is NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY, false),
+    new SimpleScenario('is POSITIVE_INFINITY', Number.POSITIVE_INFINITY, false),
 ];
 
 module.exports = FinitePositiveNumber;
